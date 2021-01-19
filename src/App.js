a79a50f77959cd5a7c133806f6a70a97759795a6
ssnodgrass <github@ssnodgrass.com>
2021-01-19 11:01:45 -0500

    Redux Store & Provider Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useEffect, Fragment } from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
@@ -18,17 +22,19 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
