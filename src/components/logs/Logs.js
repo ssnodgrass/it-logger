5383b0e806bf40b6d2268b269671143a6dadebee
ssnodgrass <github@ssnodgrass.com>
2021-01-19 12:15:10 -0500

    Connecting Redux To a Component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState([false]);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch('/logs');
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || !logs) {
     return <Preloader />;
   }
 
@@ -30,7 +21,7 @@ const Logs = () => {
         <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p classNAme='center'>No logs to show...</p>
+        <p className='center'>No logs to show...</p>
       ) : (
         logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
@@ -38,4 +29,12 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
